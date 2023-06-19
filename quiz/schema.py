a95a1c99b4caa58d1c993fa734626d04bd194595
Pere <gpere800@gmail.com>
2023-06-19 04:23:55 -0700

    modified:   quiz/__pycache__/schema.cpython-311.pyc 	modified:   quiz/schema.py

--- quiz/schema.py
+++ quiz/schema.py
@@ -110,6 +110,20 @@ class CategoryMutation(graphene.Mutation):
         category.save()
         return CategoryMutation(category=category)
         
+class CategoryMutation(graphene.Mutation):
+    
+    class Arguments:
+        id = graphene.ID()
+        name = graphene.String(required=True)
+    category = graphene.Field(CategoryType)
+        
+    @classmethod
+    def mutate(cls, root, info, name, id):
+        category = Category.objects.get(id=id)
+        category.name = name
+        category.save()
+        return CategoryMutation(category=category)
+        
               
 class Mutation(graphene.ObjectType):
     
